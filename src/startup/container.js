7482a5d39c51777b35ce54bb34a87d4e0aa1b8f7
Jhonatan Mejia <quenandres@gmail.com>
2020-05-26 21:36:23 -0500

    [ADD]: Models add

--- src/startup/container.js
+++ src/startup/container.js
@@ -13,6 +13,9 @@ const { HomeController } = require('../controllers');
 const { HomeRoutes } = require('../routes/index.routes');
 const Routes = require('../routes');
 
+//Models
+const { Comment, User, Idea } = require('../models');
+
 const container = createContainer();
 
 container
@@ -27,6 +30,10 @@ container
         HomeController: asClass(HomeController.bind(HomeController)).singleton()
     }).register({
         HomeRoutes: asFunction(HomeRoutes).singleton()
-    })
+    }).register({
+        User: asValue(User),
+        Idea: asValue(Idea),
+        Comment: asValue(Comment)
+    });
 
 module.exports = container;
